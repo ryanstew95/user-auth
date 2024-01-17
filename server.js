23a4aa4b59b8b7e624c50f370275c82b5e17985d
Ryan Stewart <ryanstew17@gmail.com>
2024-01-17 12:22:09 -0400

    worked on edge cases for login

--- server.js
+++ server.js
@@ -5,10 +5,57 @@ const morgan = require('morgan');
 const app = express();
 const port = 8001;
 
+// configuration
+app.set('view engine', 'ejs');
+
 // middleware
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
 
+// data
+const users = {
+  abc: {
+    id: "abc",
+    username: "ryanstew",
+    password: "123",
+  },
+  def: {
+    id: "def",
+    username: "bob",
+    password: "456",
+  },
+};
+
+// login endpoints
+app.get('/login', (req, res) => {
+res.render('login');
+});
+
+app.post('/login', (req, res) => {
+const username = req.body.username;
+const password = req.body.password;
+
+if (!username || !password) {
+  return res.status(400).send('you must provide a username and password');
+}
+let foundUser = null;
+for (const userId in users) {
+  const user = users[userId];
+  if (user.username == username) {
+    foundUser = user;
+  }
+}
+if (!foundUser) {
+  return res.status(400).send('no user with that username found');
+}
+if (foundUser.password !== password) {
+  return res.status(400).send('the passwords do not match');
+}
+
+
+});
+
 app.listen(port, () => {
 console.log(`app is listening on port ${port}`);
 });
+
